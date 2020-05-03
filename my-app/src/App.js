d72551b897ea720ba9c4f2101fd540d4e196fb05
Swazey Jessup <swazey_jessup@hotmail.com>
2020-05-03 17:39:26 -0600

    Characters render

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Wrapper from "./components/Wrapper";
+import CharCard from "./components/CharCard";
+import characters from "./characters.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+
+  state = {
+    characters
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+      <Wrapper>
+        {this.state.characters.map(char => (
+          <CharCard
+            id={char.id}
+            name={char.name}
+            image={char.image}
+          />
+        ))}
+      </Wrapper>
+
+
+    
+  }
 }
 
 export default App;
